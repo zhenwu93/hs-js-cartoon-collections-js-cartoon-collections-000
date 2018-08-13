495e4a1cd7eac1ce79364a53bbb7c30beec1f7a0
Zhen Wei Wu <zhenwu93@gmail.com>
2018-08-13 23:36:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 function dwarfRollCall(dwarves) {
+  var names = "";
+
+    for (var i = 0; i < dwarves.length; i++) {
+      var dwarf = dwarves[i];
+      var indexCount = i + 1;
+      names += indexCount + '. ' + dwarf + ' ';
+    }
+
+    return names;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var eleArr = [];
+
+  for (var i = 0 ; i < planeteerCalls.length; i++) {
+    var element = planeteerCalls[i];
+    eleArr.push(element.toUpperCase() + '!');
+  }
+
+  return eleArr;
 }
 
 function longPlaneteerCalls(words) {
+
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    if (word.length > 4) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function findTheCheese (foods) {
+
+  for (var i = 0; i < foods.length; i++) {
+
+    var food = foods[i];
+    if (food === 'cheddar' || food === 'gouda' || food === 'camembert') {
+      return food;
+    }
+  }
+
+  return 'no cheese!';
 }
